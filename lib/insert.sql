2abc5ddaf463c1c1af6b761e283be0bd2d0917e4
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-26 13:38:27 -0500

    commit tree 6a450e743d5f64dbc891125da958f63b6b8841b3

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate",1,"M","Blue","gucci",TRUE),
+("Rowdy",1,"M","Blue","gucci",TRUE),
+("Tabitha",1,"M","Blue","gucci",TRUE),
+("Sergeant Brown",1,"M","Blue","gucci",TRUE),
+("Melissa",1,"M","Blue","gucci",TRUE),
+("Grinch",1,"M","Blue","gucci",TRUE),
+("Wendy",1,"M","Blue","gucci",TRUE),
+(NULL,1,"M","Blue","gucci",TRUE);
